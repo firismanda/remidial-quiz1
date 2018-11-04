d89575d52382ebf59853fc31e4d8648cfedaf6bb
Muhammad Iqbal Kharis Firismanda <ryuka.1111@gmail.com>
2018-11-04 12:51:22 +0700

    Make dosen.java class. 	- Make some attributes. 	- Make the constructor. 	- Make Getter and Setter. done!

--- src/dosen.java
+++ src/dosen.java
@@ -16,7 +16,21 @@ public class dosen {
         this.namaDosen = namaDosen;
         this.nip = nip;
     }
-    
-    
+
+    public String getNamaDosen() {
+        return namaDosen;
+    }
+
+    public void setNamaDosen(String namaDosen) {
+        this.namaDosen = namaDosen;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
     
 }
